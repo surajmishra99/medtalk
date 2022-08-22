0879d980153474ac17fdd05e44d961be07d9c080
surajmishra99 <suraj.mishra9049@gmail.com>
2022-08-22 17:48:33 +0530

    User Service impl is completed and in User Controller post method is completed

---
+++ src/main/java/com/medtalk/org/controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.medtalk.org.controllers;
+
+import com.medtalk.org.payloads.UserDto;
+import com.medtalk.org.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @PostMapping("/test")
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto)
+    {
+        UserDto createUserDto = this.userService.createUser(userDto);
+        return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/medtalk/org/services/impl/UserServiceImpl.java
+++ src/main/java/com/medtalk/org/services/impl/UserServiceImpl.java
@@ -1,15 +1,18 @@
 package com.medtalk.org.services.impl;
 
 import com.medtalk.org.entity.User;
+import com.medtalk.org.exceptions.ResourceNotFoundException;
 import com.medtalk.org.payloads.UserDto;
 import com.medtalk.org.repository.UserRepo;
 import com.medtalk.org.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
@@ -25,22 +28,41 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto updateUser(UserDto userDto, Integer userId){
-//        User user = this.userRepo.findById(userId).orElseThrow((e-> new ResourceNotFoundException("user","id")));
-        return null;
+        User user = this.userRepo.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("user","id",userId));
+
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        user.setAbout(userDto.getAbout());
+
+        User updatedUser = this.userRepo.save(user);
+        UserDto userDto1= this.userToDto(updatedUser);
+
+
+        return userDto1;
     }
 
     @Override
     public UserDto getUserById(Integer userId){
-        return null;
+        User user = this.userRepo.findById(userId)
+                .orElseThrow(()->new ResourceNotFoundException("User"," Id ",userId));
+
+        return this.userToDto(user);
     }
 
     @Override
     public List<UserDto> getAllUsers(){
-        return null;
+        List<User> users = this.userRepo.findAll();
+        List<UserDto> userDtos = users.stream().map(user -> this.userToDto(user)).collect(Collectors.toList());
+        return userDtos;
     }
 
     @Override
     public void deleteUser(Integer userId){
+        User user= this.userRepo.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("User"," Id ",userId));
+        this.userRepo.delete(user);
     }
 
     private User dtoToUser (UserDto userDto)
